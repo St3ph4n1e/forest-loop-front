47cf947590083b2c146d68eae2fd7cd323a908b9
St3ph4n1e <stephanie.boisne-garnier@edu.esiee-it.fr>
2024-10-16 14:40:53 +0200

    Add axios and socketIO to front

--- src/assets/vue/pages/Home/Home.vue
+++ src/assets/vue/pages/Home/Home.vue
@@ -1,15 +1,43 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue';
 import { useRouter } from 'vue-router';
+// import { fetchData } from '@/axios/api';
+// import socket from '@/socket-io/socket'; 
+// --> socketIO = commentaires *******
 
 const router = useRouter();
 const isLoading = ref(true); 
+// const message = ref('');
 
 const goToWaitingArea = () => {
   router.push('/wait'); 
 };
 
+// const getData = async () => {
+//   try {
+//     const data = await fetchData();
+//     message.value = data.message; 
+//   } catch (error) {
+//     console.error('Error getting data:', error);
+//   }
+// };
+
+
+//*********************** */
+// const sendMessage = () => {
+//   socket.emit('message', { content: 'Hello from Vue!' });
+// };
+
 onMounted(() => {
+
+  // getData();
+
+
+//********************* */
+  // socket.on('message', (data) => {
+  //   message.value = data.response; 
+  // });
+
   setTimeout(() => {
     isLoading.value = false; 
   }, 3500); 
@@ -17,7 +45,11 @@ onMounted(() => {
 </script>
 
 <template>
+   <!-- <div>
+        <h1>{{ message }}</h1>
+      </div> -->
     <section class="home">
+      <input  type="text">
       <div v-if="isLoading" class="overlay"></div> 
       <div class="content">
         <h1 class="text-6xl font-bold text-white">Forest Loop</h1>
---
+++ src/axios/api.ts
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://localhost:3000/api', 
+});
+
+
+
+// export const fetchData = async () => {
+//     try {
+//       const response = await api.get('/test'); 
+//       return response.data;
+//     } catch (error) {
+//       console.error('Error fetching data:', error);
+//       throw error;
+//     }
+//   };
+  
