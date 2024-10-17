e03a5eef5e3f544290c2b6af83a0f78f7ddf4657
St3ph4n1e <stephanie.boisne-garnier@edu.esiee-it.fr>
2024-10-17 17:00:03 +0200

    modal component

---
+++ src/assets/vue/components/Modal/Modal.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+const emit = defineEmits(['close'])
+defineProps({
+  title: {
+    type: String,
+    default: null,
+  },
+  description: {
+    type: String,
+    default: null,
+  },
+  showModal: {
+    type: Boolean,
+    default: false,
+  },
+})
+
+const closeModal = () => {
+  emit('close')
+}
+</script>
+
+<template>
+  <section class="fl-modal" v-if="showModal">
+    <div class="modal-body flex flex-col justify-between">
+      <div class="modal-header text-center">
+        <h2>{{ title }}</h2>
+        <span @click="closeModal" class="close-btn">&times;</span>
+      </div>
+      <hr />
+      <div class="modal-content">
+        <p>{{ description }}</p>
+      </div>
+      <hr />
+      <footer class="modal-footer">
+        <button @click="closeModal">C'est Compris</button>
+      </footer>
+    </div>
+  </section>
+</template>
+
+<style src="./Modal.css" lang="css" scoped></style>
--- src/assets/vue/pages/WaitingArea/WaitingArea.vue
+++ src/assets/vue/pages/WaitingArea/WaitingArea.vue
@@ -1,41 +1,36 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import Loader from '../../components/Loader.vue';
-import { useRouter } from 'vue-router';
-import 'animate.css';
+import { ref } from 'vue'
+import Loader from '../../components/Loader.vue'
+import { useRouter } from 'vue-router'
+// import 'animate.css';
 
-const router = useRouter();
-const isLoading = ref(false);
-const code = ref('');
-const isError = ref(false);
+const router = useRouter()
+const isLoading = ref(false)
+const code = ref('')
+const isError = ref(false)
 
 const goToGame = () => {
-
-  if (code.value === '12345'){
+  if (code.value === '12345') {
     isLoading.value = true
     setTimeout(() => {
-      isLoading.value = false;
-      router.push('/game');
-    }, 2000);
+      isLoading.value = false
+      router.push('/game')
+    }, 2000)
   }
-  if (code.value !== '12345' ) {
-    isError.value= true;
-
-
+  if (code.value !== '12345') {
+    isError.value = true
   }
-};
-
-
+}
 </script>
 
 <template>
   <section class="waiting">
     <div v-if="isLoading" class="overlay"></div>
-      <div class="title">
-        <h1 class="text-6xl font-bold text-white">Forest Loop</h1>
-        <h2 class="text-2xl mt-4 text-gray-300">Plongez dans l'aventure!</h2>
-      </div>
-      <!-- <button class=" animate__animated animate__fadeOut bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+    <div class="title">
+      <h1 class="text-6xl font-bold text-white">Forest Loop</h1>
+      <h2 class="text-2xl mt-4 text-gray-300">Plongez dans l'aventure!</h2>
+    </div>
+    <!-- <button class=" animate__animated animate__fadeOut bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
         Commencer
       </button> -->
     <!-- Waiting room card -->
@@ -44,15 +39,27 @@ const goToGame = () => {
       <div class="px-6 py-4">
         <div class="font-bold text-l text-white">Code de la partie</div>
         <label for="gamecode"></label>
-        <input v-model="code" type="text" id="gamecode" class="bg-gray-50 py-4 px-10 my-8 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 text-center" maxlength="5" placeholder="Entrez un code" required />
-        <div v-if="isError" class="errorMessage  font-bold text-xs text-red-500">Veuillez réessayer avec un code valide</div>
-        <button @click="goToGame" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        <input
+          v-model="code"
+          type="text"
+          id="gamecode"
+          class="bg-gray-50 py-4 px-10 my-8 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 text-center"
+          maxlength="5"
+          placeholder="Entrez un code"
+          required
+        />
+        <div v-if="isError" class="errorMessage font-bold text-xs text-red-500">
+          Veuillez réessayer avec un code valide
+        </div>
+        <button
+          @click="goToGame"
+          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
           Valider
         </button>
-
       </div>
     </div>
-    <Loader  v-if="isLoading" />
+    <Loader v-if="isLoading" />
   </section>
 </template>
 
