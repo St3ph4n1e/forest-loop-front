9fafde1f0f3d1a1c364238afa348c3c0d0c9f409
Jlawl1et <99320146+Jlawl1et@users.noreply.github.com>
2024-10-17 10:43:12 +0200

    Update of waiting room page with link to game page

--- src/assets/vue/pages/Home/Home.vue
+++ src/assets/vue/pages/Home/Home.vue
@@ -3,31 +3,31 @@ import { ref, onMounted } from 'vue';
 import { useRouter } from 'vue-router';
 
 const router = useRouter();
-const isLoading = ref(true); 
+const isLoading = ref(true);
 
 const goToWaitingArea = () => {
-  router.push('/wait'); 
+  router.push('/wait');
 };
 
 onMounted(() => {
   setTimeout(() => {
-    isLoading.value = false; 
-  }, 4500); 
+    isLoading.value = false;
+  }, 4500);
 });
 </script>
 
 <template>
     <section class="home">
-      <div v-if="isLoading" class="overlay"></div> 
+      <div v-if="isLoading" class="overlay"></div>
       <div class="content">
-        <h1 class="text-6xl font-bold text-white">Forest Loop</h1>
-        <h2 class="text-2xl mt-4 text-gray-300">Plongez dans l'aventure!</h2>
+          <h1 class="text-6xl font-bold text-white">Forest Loop</h1>
+          <h2 class="text-2xl mt-4 text-gray-300">Plongez dans l'aventure!</h2>
         <button @click="goToWaitingArea" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
           Commencer
         </button>
       </div>
     </section>
   </template>
-  
+
 
 <style src="./Home.css" lang="css" scoped></style>
--- src/assets/vue/pages/WaitingArea/WaitingArea.vue
+++ src/assets/vue/pages/WaitingArea/WaitingArea.vue
@@ -1,39 +1,56 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue';
 import Loader from '../../components/Loader.vue';
+import { useRouter } from 'vue-router';
+import 'animate.css';
 
-const isLoading = ref(true);
+const router = useRouter();
+const isLoading = ref(false);
 const code = ref('');
 
-onMounted(() => {
+const goToGame = () => {
+  if (code.value == '12345'){
+    isLoading.value = true
+    setTimeout(() => {
+      isLoading.value = false;
+      router.push('/game');
+    }, 2000);
+  }
+
+};
+
+/*onMounted(() => {
   setTimeout(() => {
     isLoading.value = false;
   }, 2000);
-});
+});*/
 </script>
 
 <template>
   <section class="waiting">
-    <!-- Page title -->
-    <div class="title">
-      <h1 class="text-6xl font-bold text-white">Forest Loop</h1>
-      <h2 class="text-2xl mt-4 text-gray-300 pb-8">Plongez dans l'aventure!</h2>
-    </div>
-      <!-- Waiting room card -->
-      <div class="waitingCard">
-        <div class="px-6 py-4">
-          <div class="font-bold text-xl text-white ">Code de partie</div>
-          <label for="gamecode"></label>
-          <input v-model="code" type="text" id="gamecode" class="bg-gray-50 py-4 px-10 my-8 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 text-center" maxlength="5" placeholder="Entrez un code" required />
-          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            Valider
-          </button>
-        </div>
+    <div v-if="isLoading" class="overlay"></div>
+      <!-- Classe pour l'animation de déplacement vers le haut -->
+      <div class="title">
+        <h1 class="text-6xl font-bold text-white">Forest Loop</h1>
+        <h2 class="text-2xl mt-4 text-gray-300">Plongez dans l'aventure!</h2>
       </div>
-      <Loader  v-if="isLoading" />
-
+      <!-- <button class=" animate__animated animate__fadeOut bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Commencer
+      </button> -->
+    <!-- Waiting room card -->
+    <!--<div class="waitingCard animate__animated animate__fadeIn">-->
+    <div class="waitingCard">
+      <div class="px-6 py-4">
+        <div class="font-bold text-xl text-white ">Code de partie</div>
+        <label for="gamecode"></label>
+        <input v-model="code" type="text" id="gamecode" class="bg-gray-50 py-4 px-10 my-8 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 text-center" maxlength="5" placeholder="Entrez un code" required />
+        <button @click="goToGame" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Valider
+        </button>
+      </div>
+    </div>
+    <Loader  v-if="isLoading" />
   </section>
 </template>
-
 
 <style src="./WaitingArea.css" lang="css" scoped></style>
