d425a8e2af8d685bd4f4cb3d86f597d327580644
St3ph4n1e <stephanie.boisne-garnier@edu.esiee-it.fr>
2024-10-21 12:41:39 +0200

    improve css

--- src/assets/vue/pages/Rules/Rules.vue
+++ src/assets/vue/pages/Rules/Rules.vue
@@ -3,11 +3,11 @@ import Modal from '../../components/Modal/Modal.vue'
 import Header from '../../components/Header/Header.vue'
 import RuleCard from '../../components/RuleCard/RuleCard.vue'
 import { ref } from 'vue'
-import { gameRules } from './rules.const.ts'
+import { gameRules } from './rules.const'
 
 const showModal = ref(false)
 const selectedRule = ref({ titre: '', description: '' })
-const openModalWithRule = (rule: { titre: string, description: string }) => {
+const openModalWithRule = (rule: { titre: string; description: string }) => {
   selectedRule.value = rule
   showModal.value = true
 }
@@ -22,7 +22,7 @@ const handleModal = () => {
   <section class="rules p-10">
     <div v-if="!showModal" class="main-card animate__animated animate__fadeIn">
       <div class="title text-white">
-        <h1>Guide forestier</h1>
+        <h1 class="text-nowrap">Guide forestier</h1>
       </div>
       <div class="cards grid grid-cols-1 md:grid-cols-4 gap-8 p-12">
         <RuleCard
@@ -33,16 +33,14 @@ const handleModal = () => {
           @click="openModalWithRule(rule)"
         />
       </div>
-      <h1 class="text-white text-lg">
-        Séléctionne une option
-      </h1>
+      <h1 class="text-white text-lg">Séléctionne une option</h1>
     </div>
-    <Modal class="animate__animated animate__animated animate__fadeIn"
+    <Modal
+      class="animate__animated animate__animated animate__fadeIn"
       :title="selectedRule.titre"
       :description="selectedRule.description"
       :show-modal="showModal"
       @close="handleModal"
-
     />
   </section>
 </template>
