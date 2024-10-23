8b1fa9e5f0013049c62f11fe44b9a99688e47127
Jlawl1et <99320146+Jlawl1et@users.noreply.github.com>
2024-10-23 11:04:28 +0200

    Setup of room connection with sockets

--- src/assets/vue/pages/WaitingArea/WaitingArea.vue
+++ src/assets/vue/pages/WaitingArea/WaitingArea.vue
@@ -1,23 +1,124 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
-import Loader from '../../components/Loader.vue';
+import { ref, onMounted } from 'vue'
+import Loader from '../../components/Loader.vue'
+import { useRouter } from 'vue-router'
+import socket from '@/socket-io/socket'
 
-const isLoading = ref(true);
+const router = useRouter()
+const isLoading = ref(false)
+const code = ref('')
+const isError = ref(false)
+const errorMessage = ref('')
+const isCard = ref(false)
+const isButton = ref(true)
+const isMovedUp = ref(false)
+
+const goToGame = () => {
+  console.log(code.value)
+  if (!code.value) {
+    errorMessage.value = 'Veuillez entrer un code valide'
+    isError.value = true
+    return
+  }
+
+  /*isLoading.value = true
+  setTimeout(() => {
+    isLoading.value = false
+    router.push('/game')
+  }, 1500)*/
+
+
+  socket.emit('join game', code.value)
+
+
+  isLoading.value = true
+}
+
+const initValue = () => {
+  errorMessage.value = ''
+  code.value = ''
+  isError.value = false
+}
 
 onMounted(() => {
   setTimeout(() => {
-    isLoading.value = false; 
-  }, 2000);
-});
+    isMovedUp.value = true
+    isCard.value = true
+  }, 700)
+
+  isCard.value = false
+  isButton.value = true
+
+  socket.on('full room', () => {
+    console.log('full room')
+    errorMessage.value = 'La room est pleine.'
+    isError.value = true
+    isLoading.value = false
+  })
+
+  socket.on('already in room', () => {
+    console.log('already in room')
+    errorMessage.value = 'Vous êtes déjà dans une room.'
+    isError.value = true
+    isLoading.value = false
+  })
+
+  socket.on('room does not exist', () => {
+    console.log('room does not exist')
+    errorMessage.value = 'Cette room n\'existe pas.'
+    isError.value = true
+    isLoading.value = false
+  })
+
+  socket.on('join room', (room) => {
+    console.log('join room', room)
+    isLoading.value = false
+    router.push('/game')
+  })
+})
 </script>
 
 <template>
-  <div>
-    <h1 >Page d'attente</h1>
-    <p>Veuillez patienter...</p>
-    <Loader  v-if="isLoading" />
-  </div>
-</template>
+  <section id="waiting">
+    <div v-if="isLoading" class="overlay"></div>
+    <div :class="['title', { 'move-up': isMovedUp }]">
+      <h1 class="text-6xl font-bold text-white">Forest Loop</h1>
+      <h2 class="text-2xl mt-4 text-gray-300">Plongez dans l'aventure!</h2>
+      <button
+        v-if="isButton"
+        class=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Commencer
+      </button>
+    </div>
 
+    <div v-if="isCard" class="waitingCard">
+      <div class="px-6 py-4">
+        <div class="font-bold text-l text-white">Code de la partie</div>
+        <label for="gamecode"></label>
+        <input
+          v-model="code"
+          type="text"
+          id="gamecode"
+          class="bg-gray-50 py-4 px-10 my-8 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 text-center"
+          maxlength="5"
+          placeholder="Entrez un code"
+          required
+        />
+        <div v-if="isError" class="errorMessage font-bold text-xs text-red-500">
+          {{ errorMessage }}
+        </div>
+        <button
+          @blur="initValue"
+          @click="goToGame"
+          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Valider
+        </button>
+      </div>
+    </div>
+    <Loader v-if="isLoading" />
+  </section>
+</template>
 
 <style src="./WaitingArea.css" lang="css" scoped></style>
