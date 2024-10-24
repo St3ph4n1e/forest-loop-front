0e7a6e2e7241c067d12ca911f5403036db7cdf2f
St3ph4n1e <stephanie.boisne-garnier@edu.esiee-it.fr>
2024-10-24 16:42:46 +0200

    improve app  UI

--- src/assets/vue/pages/Home/Home.vue
+++ src/assets/vue/pages/Home/Home.vue
@@ -18,14 +18,14 @@ onMounted(() => {
 </script>
 
 <template>
-  <Header></Header>
+  <Header v-if="!isLoading"></Header>
   <section class="home">
     <div v-if="isLoading" class="overlay"></div>
     <div class="content">
       <div class="title">
-        <h1 class="text-center font-bold text-green-500">Forest Loop</h1>
+        <h1 class="text-center font-bold">Forest Loop</h1>
       </div>
-      <h2 class="text-2xl mt-4 text-gray-300">Plongez dans l'aventure!</h2>
+      <h2 class="text-2xl mt-4 text-white">Plongez dans l'aventure!</h2>
       <button
         @click="goToWaitingArea"
         class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
